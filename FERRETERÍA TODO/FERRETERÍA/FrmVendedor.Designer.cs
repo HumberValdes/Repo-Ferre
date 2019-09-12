dea4497eac2d1ce2d1a799c7aa0420f0efadd181
Gustavo <busigusi1@gmail.com>
2019-09-12 01:15:26 -0500

    Chino junta

--- FERRETERÍA TODO/FERRETERÍA/FrmVendedor.Designer.cs
+++ FERRETERÍA TODO/FERRETERÍA/FrmVendedor.Designer.cs
@@ -28,152 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.BtnInsertar = new System.Windows.Forms.Button();
-            this.BtnEditar = new System.Windows.Forms.Button();
-            this.BtnEliminar = new System.Windows.Forms.Button();
-            this.BtnActualizar = new System.Windows.Forms.Button();
-            this.txtVendedor = new System.Windows.Forms.TextBox();
-            this.txtNombre = new System.Windows.Forms.TextBox();
-            this.txtIDVendedor = new System.Windows.Forms.TextBox();
-            this.GrvVendedor = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.GrvVendedor)).BeginInit();
             this.SuspendLayout();
-            // 
-            // BtnInsertar
-            // 
-            this.BtnInsertar.Location = new System.Drawing.Point(30, 36);
-            this.BtnInsertar.Name = "BtnInsertar";
-            this.BtnInsertar.Size = new System.Drawing.Size(75, 23);
-            this.BtnInsertar.TabIndex = 0;
-            this.BtnInsertar.Text = "INSERTAR";
-            this.BtnInsertar.UseVisualStyleBackColor = true;
-            this.BtnInsertar.Click += new System.EventHandler(this.BtnInsertar_Click);
-            // 
-            // BtnEditar
-            // 
-            this.BtnEditar.Location = new System.Drawing.Point(30, 108);
-            this.BtnEditar.Name = "BtnEditar";
-            this.BtnEditar.Size = new System.Drawing.Size(75, 23);
-            this.BtnEditar.TabIndex = 1;
-            this.BtnEditar.Text = "EDITAR";
-            this.BtnEditar.UseVisualStyleBackColor = true;
-            this.BtnEditar.Click += new System.EventHandler(this.BtnEditar_Click);
-            // 
-            // BtnEliminar
-            // 
-            this.BtnEliminar.Location = new System.Drawing.Point(670, 36);
-            this.BtnEliminar.Name = "BtnEliminar";
-            this.BtnEliminar.Size = new System.Drawing.Size(75, 23);
-            this.BtnEliminar.TabIndex = 2;
-            this.BtnEliminar.Text = "ELIMINAR";
-            this.BtnEliminar.UseVisualStyleBackColor = true;
-            this.BtnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click);
-            // 
-            // BtnActualizar
-            // 
-            this.BtnActualizar.Location = new System.Drawing.Point(337, 330);
-            this.BtnActualizar.Name = "BtnActualizar";
-            this.BtnActualizar.Size = new System.Drawing.Size(133, 23);
-            this.BtnActualizar.TabIndex = 3;
-            this.BtnActualizar.Text = "ACTUALIZAR TABLA";
-            this.BtnActualizar.UseVisualStyleBackColor = true;
-            this.BtnActualizar.Click += new System.EventHandler(this.BtnActualizar_Click);
-            // 
-            // txtVendedor
-            // 
-            this.txtVendedor.Location = new System.Drawing.Point(156, 75);
-            this.txtVendedor.Name = "txtVendedor";
-            this.txtVendedor.Size = new System.Drawing.Size(100, 20);
-            this.txtVendedor.TabIndex = 4;
-            // 
-            // txtNombre
-            // 
-            this.txtNombre.Location = new System.Drawing.Point(323, 75);
-            this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(100, 20);
-            this.txtNombre.TabIndex = 5;
-            // 
-            // txtIDVendedor
-            // 
-            this.txtIDVendedor.Location = new System.Drawing.Point(657, 75);
-            this.txtIDVendedor.Name = "txtIDVendedor";
-            this.txtIDVendedor.Size = new System.Drawing.Size(100, 20);
-            this.txtIDVendedor.TabIndex = 6;
-            // 
-            // GrvVendedor
-            // 
-            this.GrvVendedor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.GrvVendedor.Location = new System.Drawing.Point(180, 162);
-            this.GrvVendedor.Name = "GrvVendedor";
-            this.GrvVendedor.Size = new System.Drawing.Size(474, 150);
-            this.GrvVendedor.TabIndex = 7;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(155, 56);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 13);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Iserta ID ejemplo (1)";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(278, 56);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(228, 13);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Inserta Nombre ejemplo (Juan Mendoza Garza)";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(657, 102);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(101, 13);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Iserta ID ejemplo (1)";
             // 
             // FrmVendedor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.GrvVendedor);
-            this.Controls.Add(this.txtIDVendedor);
-            this.Controls.Add(this.txtNombre);
-            this.Controls.Add(this.txtVendedor);
-            this.Controls.Add(this.BtnActualizar);
-            this.Controls.Add(this.BtnEliminar);
-            this.Controls.Add(this.BtnEditar);
-            this.Controls.Add(this.BtnInsertar);
             this.Name = "FrmVendedor";
             this.Text = "VENDEDORES";
-            ((System.ComponentModel.ISupportInitialize)(this.GrvVendedor)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button BtnInsertar;
-        private System.Windows.Forms.Button BtnEditar;
-        private System.Windows.Forms.Button BtnEliminar;
-        private System.Windows.Forms.Button BtnActualizar;
-        private System.Windows.Forms.TextBox txtVendedor;
-        private System.Windows.Forms.TextBox txtNombre;
-        private System.Windows.Forms.TextBox txtIDVendedor;
-        private System.Windows.Forms.DataGridView GrvVendedor;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
     }
 }
